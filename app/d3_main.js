c1254555fcbd95f3f83fb37f12114d10f7611db3
Peter Griggs <petergriggs17@gmail.com>
2019-04-11 03:41:01 -0400

    Coloring nodes on grabbing

--- app/d3_main.js
+++ app/d3_main.js
@@ -40,113 +40,110 @@ var nodes = [
       { target: "pike"  , source: "cat" , strength: 0.1 }
   ]
 
-var setupUserInterface = function() {
     
-      function getNeighbors(node) {
-        return links.reduce(function (neighbors, link) {
-            if (link.target.id === node.id) {
-              neighbors.push(link.source.id)
-            } else if (link.source.id === node.id) {
-              neighbors.push(link.target.id)
-            }
-            return neighbors
-          },
-          [node.id]
-        )
-      }
-      function isNeighborLink(node, link) {
-        return link.target.id === node.id || link.source.id === node.id
-      }
-      function getNodeColor(node, neighbors) {
-        if (Array.isArray(neighbors) && neighbors.indexOf(node.id) > -1) {
-          return node.level === 1 ? 'blue' : 'green'
+  function getNeighbors(node) {
+    return links.reduce(function (neighbors, link) {
+        if (link.target.id === node.id) {
+          neighbors.push(link.source.id)
+        } else if (link.source.id === node.id) {
+          neighbors.push(link.target.id)
         }
-        return node.level === 1 ? 'red' : 'gray'
-      }
-      function getLinkColor(node, link) {
-        return isNeighborLink(node, link) ? 'green' : '#E5E5E5'
-      }
-      function getTextColor(node, neighbors) {
-        return Array.isArray(neighbors) && neighbors.indexOf(node.id) > -1 ? 'green' : 'black'
-      }
-      var width = window.innerWidth
-      var height = window.innerHeight
-      var svg = d3.select('svg')
-      svg.attr('width', width).attr('height', height)
-      // simulation setup with all forces
-      var linkForce = d3
-        .forceLink()
-        .id(function (link) { return link.id })
-        .strength(function (link) { return link.strength })
-      var simulation = d3
-        .forceSimulation()
-        .force('link', linkForce)
-        .force('charge', d3.forceManyBody().strength(-120))
-        .force('center', d3.forceCenter(width / 2, height / 2))
-      var dragDrop = d3.drag().on('start', function (node) {
-        node.fx = node.x
-        node.fy = node.y
-      }).on('drag', function (node) {
-        simulation.alphaTarget(0.7).restart()
-        node.fx = d3.event.x
-        node.fy = d3.event.y
-      }).on('end', function (node) {
-        if (!d3.event.active) {
-          simulation.alphaTarget(0)
-        }
-        node.fx = null
-        node.fy = null
-      })
-      function selectNode(selectedNode) {
-        var neighbors = getNeighbors(selectedNode)
-        // we modify the styles to highlight selected nodes
-        nodeElements.attr('fill', function (node) { return getNodeColor(node, neighbors) })
-        // textElements.attr('fill', function (node) { return getTextColor(node, neighbors) })
-        linkElements.attr('stroke', function (link) { return getLinkColor(selectedNode, link) })
-      }
-      var linkElements = svg.append("g")
-        .attr("class", "links")
-        .selectAll("line")
-        .data(links)
-        .enter().append("line")
-          .attr("stroke-width", 5)
-            .attr("stroke", "rgba(50, 50, 50, 0.2)")
-      var nodeElements = svg.append("g")
-        .attr("class", "nodes")
-        .selectAll("circle")
-        .data(nodes)
-        .enter().append("circle")
-          .attr("r", 30)
-          .attr("fill", getNodeColor)
-          .call(dragDrop)
-          .on('click', selectNode)
-      // var textElements = svg.append("g")
-      //   .attr("class", "texts")
-      //   .selectAll("text")
-      //   .data(nodes)
-      //   .enter().append("text")
-      //     .text(function (node) { return  node.label })
-      // 	  .attr("font-size", 15)
-      // 	  .attr("dx", 15)
-      //     .attr("dy", 4)
-      simulation.nodes(nodes).on('tick', () => {
-        nodeElements
-          .attr('cx', function (node) { return node.x })
-          .attr('cy', function (node) { return node.y })
-        // textElements
-        //   .attr('x', function (node) { return node.x })
-        //   .attr('y', function (node) { return node.y })
-        linkElements
-          .attr('x1', function (link) { return link.source.x })
-          .attr('y1', function (link) { return link.source.y })
-          .attr('x2', function (link) { return link.target.x })
-          .attr('y2', function (link) { return link.target.y })
-      })
-      simulation.force("link").links(links)
-};
+        return neighbors
+      },
+      [node.id]
+    )
+  }
+  function isNeighborLink(node, link) {
+    return link.target.id === node.id || link.source.id === node.id
+  }
+  function getNodeColor(node, neighbors) {
+    if (Array.isArray(neighbors) && neighbors.indexOf(node.id) > -1) {
+      return node.level === 1 ? 'blue' : 'green'
+    }
+    return node.level === 1 ? 'gray' : 'gray'
+  }
+  function getLinkColor(node, link) {
+    return isNeighborLink(node, link) ? 'green' : '#E5E5E5'
+  }
+  function getTextColor(node, neighbors) {
+    return Array.isArray(neighbors) && neighbors.indexOf(node.id) > -1 ? 'green' : 'black'
+  }
+  var width = window.innerWidth
+  var height = window.innerHeight
+  var svg = d3.select('svg')
+  svg.attr('width', width).attr('height', height)
+  // simulation setup with all forces
+  var linkForce = d3
+    .forceLink()
+    .id(function (link) { return link.id })
+    .strength(function (link) { return link.strength })
+  var simulation = d3
+    .forceSimulation()
+    .force('link', linkForce)
+    .force('charge', d3.forceManyBody().strength(-120))
+    .force('center', d3.forceCenter(width / 2, height / 2))
+  var dragDrop = d3.drag().on('start', function (node) {
+    node.fx = node.x
+    node.fy = node.y
+  }).on('drag', function (node) {
+    simulation.alphaTarget(0.7).restart()
+    node.fx = d3.event.x
+    node.fy = d3.event.y
+  }).on('end', function (node) {
+    if (!d3.event.active) {
+      simulation.alphaTarget(0)
+    }
+    node.fx = null
+    node.fy = null
+  })
+  function selectNode(selectedNode) {
+    var neighbors = getNeighbors(selectedNode)
+    // we modify the styles to highlight selected nodes
+    nodeElements.attr('fill', function (node) { return getNodeColor(node, neighbors) })
+    // textElements.attr('fill', function (node) { return getTextColor(node, neighbors) })
+    linkElements.attr('stroke', function (link) { return getLinkColor(selectedNode, link) })
+  }
+  var linkElements = svg.append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(links)
+    .enter().append("line")
+      .attr("stroke-width", 5)
+        .attr("stroke", "rgba(50, 50, 50, 0.2)")
+  var nodeElements = svg.append("g")
+    .attr("class", "nodes")
+    .selectAll("circle")
+    .data(nodes)
+    .enter().append("circle")
+      .attr("r", 30)
+      .attr("fill", getNodeColor)
+      .call(dragDrop)
+      .on('click', selectNode)
+  // var textElements = svg.append("g")
+  //   .attr("class", "texts")
+  //   .selectAll("text")
+  //   .data(nodes)
+  //   .enter().append("text")
+  //     .text(function (node) { return  node.label })
+  // 	  .attr("font-size", 15)
+  // 	  .attr("dx", 15)
+  //     .attr("dy", 4)
+  simulation.nodes(nodes).on('tick', () => {
+    nodeElements
+      .attr('cx', function (node) { return node.x })
+      .attr('cy', function (node) { return node.y })
+    // textElements
+    //   .attr('x', function (node) { return node.x })
+    //   .attr('y', function (node) { return node.y })
+    linkElements
+      .attr('x1', function (link) { return link.source.x })
+      .attr('y1', function (link) { return link.source.y })
+      .attr('x2', function (link) { return link.target.x })
+      .attr('y2', function (link) { return link.target.y })
+  })
+  simulation.force("link").links(links)
 
 
-setupUserInterface();
 var isGrabbing = false;
 var cursorPosition = [0,0];
 var screenPosition = [0,0];
@@ -192,37 +189,35 @@ Leap.loop({ hand: function(hand) {
         }
         
 
-        
-        d3.selectAll('circle').each(function(d) {
-          // Logs the cx and cy attributes of a node.
-            if (d3.select(this).attr('id') != 'cursor') {
-              // x = d3.select(this).attr('cx')
-              // y = d3.select(this).attr('cy')
-              x = d.x;
-              y = d.y;
-              // console.log("james is tired");
-              // console.log(x,y);
-              var bound = 30;
-              var bbox = {
-                  x1: x-bound,
-                  x2: x+bound,
-                  y1: y-bound,
-                  y2: y+bound,
-              };
-              // console.log("bbox: " + JSON.stringify(bbox));
-              // console.log("cursor pos: " + cursorPosition);
-              if (bbox.x1 <= cursorPosition[0] && bbox.x2 >= cursorPosition[0]
-                && bbox.y1 <= cursorPosition[1] && bbox.y2 >= cursorPosition[1]) {
-                  if (intersectNode == false){
+        if (intersectNode == false){
+          d3.selectAll('circle').each(function(d) {
+            // Logs the cx and cy attributes of a node.
+              if (d3.select(this).attr('id') != 'cursor') {
+                // x = d3.select(this).attr('cx')
+                // y = d3.select(this).attr('cy')
+                x = d.x;
+                y = d.y;
+                // console.log("james is tired");
+                // console.log(x,y);
+                var bound = 30;
+                var bbox = {
+                    x1: x-bound,
+                    x2: x+bound,
+                    y1: y-bound,
+                    y2: y+bound,
+                };
+                // console.log("bbox: " + JSON.stringify(bbox));
+                // console.log("cursor pos: " + cursorPosition);
+                if (bbox.x1 <= cursorPosition[0] && bbox.x2 >= cursorPosition[0]
+                  && bbox.y1 <= cursorPosition[1] && bbox.y2 >= cursorPosition[1]) {
                     initialPosition = [x, y];
-                  }
-                  intersectNode = d;
-                  console.log("intersects");
+                    intersectNode = d;
+                    d3.select(this).attr('fill', 'lightgreen');
+                    console.log("intersects");
+                }
               }
-            }
-
-            
-        });
+            });
+          }
 
         // ctz += 1;
         // console.log("after for loop");
@@ -232,8 +227,13 @@ Leap.loop({ hand: function(hand) {
         // console.log("inside if statement");
         intersectNode.x = initialPosition[0];
         intersectNode.y = initialPosition[1];
+        isGrabbing = false;
+        cursorPosition = [0,0];
+        screenPosition = [0,0];
         intersectNode = false;
+        initialPosition = false;
       }
-    }
-    console.log("insersect node is: " + intersectNode);
+    }intersectNode.x = initialPosition[0];
+    // intersectNode.y = initialPosition[1];
+    console.log("insersect node is: " + JSON.stringify(intersectNode));
 }}).use('screenPosition', {scale: LEAPSCALE});
